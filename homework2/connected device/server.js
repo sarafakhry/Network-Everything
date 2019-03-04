5aed17ac6a6a0e1ae0f3a145cf2697034a115c64
sarafakhry <36405292+sarafakhry@users.noreply.github.com>
2019-03-04 21:39:55 +0400

    Create server.js

---
+++ homework2/connected device/server.js
@@ -0,0 +1,15 @@
+const http = require('http')
+const fs = require('fs');
+const express = require('express'); // web server application
+const app = express();        // instantiate express server
+const server = http.Server(app);  // connects http library to server
+const hostname = "127.0.0.1";
+
+
+const PORT=8080;
+
+app.use(express.static('public'));  // find pages in public directory
+
+server.listen(PORT, () => {
+  console.log(`Server running at http://${hostname}:${PORT}/`);
+})
