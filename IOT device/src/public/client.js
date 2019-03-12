7a1d291d1571040b98c727be07d3ab1c081af7a2
sarafakhry <36405292+sarafakhry@users.noreply.github.com>
2019-03-12 12:59:44 +0400

    Update client.js

--- IOT device/src/public/client.js
+++ IOT device/src/public/client.js
@@ -1,1 +1,8 @@
+var socket = io();
 
+
+function ledON() {
+  console.log ("sending a message to the web server to turn on the LED");
+  // this is how we send a message back to the web server
+  socket.emit('ledON'); 
+}
